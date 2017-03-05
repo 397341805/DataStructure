3abcad014ddc7c31f1eeed7c607a875e01ecff9c
天皓 刘 <lth1@qq.com>
2017-03-05 20:22:22 +0800

    2017.3.5 夜

---
+++ ConsoleApplication1/function.cpp
@@ -0,0 +1,59 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include"linelist.h"
+#include<string>
+void InitList(LINE &L,int length)
+{
+	L.Size = length;
+	L.Length = 0;
+	L.sp = (STUFF*)malloc(sizeof(STUFF)*length);
+}
+bool addElement(LINE &L, STUFF &S)
+{
+	if (L.Length >= L.Size)
+	{
+		return -1;
+	}
+	L.sp[L.Length] = S;
+	L.Length++;
+	return 1;
+}
+bool delElement(LINE &L, int n)
+{
+	if (n >= L.Length)
+	{
+		return -1;
+	}
+	for (int i = n;i < L.Length-1;i++)
+	{
+		L.sp[i] = L.sp[i + 1];
+	}
+	L.Length--;
+}
+int findElement(LINE &L, int num)
+{
+	for (int i = 0;i < L.Length;i++)
+	{
+		if (L.sp[i].Jobnum == num)
+			return i;
+	}
+	return -1;
+}
+int findElement(LINE &L, char *name)
+{
+	for (int i = 0;i < L.Length;i++)
+	{
+		if (strcmp(L.sp[i].name, name) > 0)
+			return i;
+		
+	}
+	return -1;
+}
+void printLineList(LINE &L)
+{
+	printf("打印线性表：\n");
+	for (int i = 0;i < L.Length;i++)
+	{
+		printf("%d 姓名: %s 工号： %d 职务: %s /n", i + 1,&L.sp[i].name, &L.sp[i].Jobnum, &L.sp[i].duty);
+	}
+}
--- ConsoleApplication1/linelist.h
+++ ConsoleApplication1/linelist.h
@@ -1,2 +1,30 @@
 #pragma once
+/*
+某软件公司大约有30名员工，每名员工有姓名、工号、职务等属性，每年都有员工离职和入职。把所有员工建立一个线性表，建立离职和入职函数，当有员工离职或入职时，修改线性表，并且显示最新的员工名单。
+要求:1)顺序表存储；实现顺序表的插入、删除、查找、输出等基本操作；调用基本操作完成。2)链表存储；实现链表的插入、删除、查找、输出等基本操作；调用基本操作完成。
+*/
 //数据结构 线性表
+/*建立线性表 属性有姓名、工号、职务*/
+struct LINE
+{
+	STUFF *sp;
+	int Length;
+	int Size;
+};
+struct STUFF{
+	char name[10];
+	int Jobnum;
+	char duty[10];
+
+};
+//初始化顺序表
+void  InitList(LINE &L);
+//添加元素
+bool addElement(LINE &L, STUFF &S);
+//删除元素
+bool delElement(LINE &L, int n);
+//查找
+int findElement(LINE &L, int num);
+int findElement(LINE &L, char *name);
+//输出
+void printLineList(LINE &L);
